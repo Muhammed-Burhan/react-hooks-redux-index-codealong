3d7130c794e9c5c965210b469df1b15b6f563680
Muhammed <muhammadburhan964@gmail.com>
2021-09-01 00:59:30 +0300

    commit tree 28ea3d4bae6a6fe762aa8692973811b5c31c70a1

--- src/features/todos/TodoContainer.js
+++ src/features/todos/TodoContainer.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
+import Todo from "./Todo";
 function TodoContainer() {
-  return <div>TodoContainer</div>;
+  const todos = useSelector((state) => state.todos.entities);
+
+  const todoList = todos.map((todo, index) => <Todo key={index} text={todo} />);
+  return (
+    <div>
+      TodoContainer
+      <ul>{todoList}</ul>
+    </div>
+  );
 }
 
 export default TodoContainer;
